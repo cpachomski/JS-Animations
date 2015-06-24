ad33d59a2534f66f6bdab277a52cd491354cf797
Chris Pachomski <chrisp@ironpaperweight.private>
2015-06-24 12:10:11 -0400

    add velocity function for squishing box and fading text

--- js/app.js
+++ js/app.js
@@ -1,49 +1,30 @@
-console.log('connected');
+console.log('Tesing 1 2 1 2');
 
 
-$('#to-yellow').click(function(){
-
-  $('#div4').velocity("scroll", 1000);
-});
 
 
-$('#to-red').click(function(){
-
-  $('#div4').velocity('reverse')
-
-});
-
-$('#box-1').click(function(){
-
-  $('#box-1').velocity(
-    {
-      width: "0px",
-      height:'0px',
-      rotateZ: -180,
-      opacity: 1
-    },
-    {
-      duration:1000,
-      easing: 'swing'
-    }).velocity(
-    {
-      width:'500px',
-      height: '200px'
-    },
-    {
-      duration:2000,
-      easing: 'ease-in-out'
-    }
-   );
-
-
+$('#show-text-btn').click(function(){
+  $('p').velocity(
+  {
+    opacity:1
+  },
+  {
+    duration: 2000,
+    easing: 'ease-out'
+  })
 
 });
 
+$('#squish-box-btn').click(function(){
+  $('#squish-box').velocity(
+      {
+        width: '100px'
 
+      },
+      {
+        duration:1000,
+        easing:'spring'
+      }
+    )
 
-
-
-
-
-
+});
